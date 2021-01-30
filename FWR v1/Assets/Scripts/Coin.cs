7ef541996e6e8c449dfb1cc82cea33bfb4376f6b
Dany Karam <danykaram97@gmail.com>
2021-01-29 22:05:17 -0400

    uploading

---
+++ FWR v1/Assets/Scripts/Coin.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+	
+	public static int coinsCount = 0;
+	public AudioSource coinsource;
+	public AudioClip clipcoin;
+	public AudioClip clipfirework;
+
+	// Use this for initialization
+	void Start () {
+		Coin.coinsCount++;
+		//Debug.Log ("hola");
+		coinsource=GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider collider){
+		if (collider.CompareTag ("Player")) {
+			AudioSource.PlayClipAtPoint (clipcoin, transform.position);
+			Debug.Log ("Te faltan por recolectar " + (coinsCount-1).ToString () + " monedas.");
+			Destroy (gameObject);
+		}
+	}
+	void OnDestroy(){
+		Coin.coinsCount--;
+		if (Coin.coinsCount <= 0) {
+			GameObject timer = GameObject.Find ("GameTimer");
+
+
+
+			Debug.Log ("Ganaste!! :)");
+			GameObject[] fireworks = GameObject.FindGameObjectsWithTag ("Firework");
+			foreach (GameObject firework in fireworks) {
+				firework.GetComponent<ParticleSystem> ().Play ();
+			}
+			Destroy (timer);
+		}
+	
+	}
+
+}
---
+++ FWR v1/Assets/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+	public float maxTime= 60.0f;
+	private float countdown=0.0f;
+	public AudioSource audiosource;
+
+
+	// Use this for initialization
+	void Start () {
+		countdown = maxTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		countdown -= Time.deltaTime;
+		if (countdown <= 0) {
+			Coin.coinsCount = 0;
+			SceneManager.LoadScene ("Level_01");
+		}
+	}
+	void onDisable(){
+	
+//		audiosource.clip = clipfirework;
+//		AudioSource.PlayClipAtPoint (clipfirework, transform.position);
+	}
+}
